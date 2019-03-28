6654cc7641564601d5ff6dea88a8c36b7fdb5d41
supercoolsonic <joeyj95@hotmail.com>
2019-03-28 11:41:53 -0500

    initial commit

---
+++ AutoDemo_RedMountain/MiscFiles/StartPos.h
@@ -0,0 +1,10 @@
+#include "SADXModLoader.h"
+
+/*
+StartPosition sonicRedMountain0 = { LevelIDs_RedMountain, 0,{ 256, 60, -885 }, 0 };
+StartPosition sonicRedMountain1 = { LevelIDs_RedMountain, 1,{ 694, -113, -205 }, 0x8000 };
+StartPosition gammaRedMountain1 = { LevelIDs_WindyValley, 1,{ 0, 700, 0 }, 0x671C }; //0, 700, 0
+
+
+StartPosition knucklesRedMountain2 = { LevelIDs_RedMountain, 2,{ 0, 200, 0 }, 0x6AAA };
+*/
